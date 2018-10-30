02591d460e576ddafe54fa3644cd3cef815638bc
Kiran <keer4n@gmail.com>
2018-10-30 02:26:15 +0000

    working version

--- src/ttt/Game.java
+++ src/ttt/Game.java
@@ -6,57 +6,67 @@ import ttt.Game.GameState;
 import ttt.Game.Seed;
 
 public class Game {
-	
+
 	private final int ROWS = 3;
 	private final int COLS = 3;
-	
-	 public enum GameState {
-	      PLAYING, DRAW, CROSS_WON, NOUGHT_WON
-	   }
-	   private GameState currentState;  // the current game state
-	 
-	   // Use an enumeration (inner class) to represent the seeds and cell contents
-	   public enum Seed {
-	      EMPTY, CROSS, NOUGHT
-	   }
-	   private Seed currentPlayer;  // the current player
-	 
-	   private Seed[][] board   ; // Game board of ROWS-by-COLS cells
-	 
-	   public void Game() {
-		   board = new Seed[ROWS][COLS]; // allocate array
-		   initGame(); // initialize the game board contents and game variables
-	   }
-	   
-	   public int getRows() {
-		   return this.ROWS;
-	   }
-	   
-	   public int getCols() {
-		   return this.COLS;
-	   }
-	   public void initGame() {
-		      for (int row = 0; row < ROWS; ++row) {
-		         for (int col = 0; col < COLS; ++col) {
-		            board[row][col] = Seed.EMPTY; // all cells empty
-		         }
-		      }
-		      currentState = GameState.PLAYING; // ready to play
-		      currentPlayer = Seed.CROSS;       // cross plays first
-		   }
-	   
-	   /** Update the currentState after the player with "theSeed" has placed on
+
+	public enum GameState {
+		PLAYING, DRAW, CROSS_WON, NOUGHT_WON
+	}
+	private GameState currentState;  // the current game state
+
+	// Use an enumeration (inner class) to represent the seeds and cell contents
+	public enum Seed {
+		EMPTY, CROSS, NOUGHT
+	}
+	private Seed currentPlayer;  // the current player
+
+	private Seed[][] board   ; // Game board of ROWS-by-COLS cells
+
+	public void Game() {
+		board = new Seed[ROWS][COLS]; // allocate array
+		initGame(); // initialize the game board contents and game variables
+	}
+
+	public int getRows() {
+		return this.ROWS;
+	}
+
+	public int getCols() {
+		return this.COLS;
+	}
+	public void initGame() {
+		for (int row = 0; row < ROWS; ++row) {
+			for (int col = 0; col < COLS; ++col) {
+				board[row][col] = Seed.EMPTY; // all cells empty
+			}
+		}
+		currentState = GameState.PLAYING; // ready to play
+		currentPlayer = Seed.CROSS;       // cross plays first
+	}
+
+	/** Update the currentState after the player with "theSeed" has placed on
        (rowSelected, colSelected). */
-   public void updateState(Seed theSeed, int rowSelected, int colSelected) {
-      if (hasWon(theSeed, rowSelected, colSelected)) {  // check for win
-         currentState = (theSeed == Seed.CROSS) ? GameState.CROSS_WON : GameState.NOUGHT_WON;
-      } else if (isDraw()) {  // check for draw
-         currentState = GameState.DRAW;
-      }
-      // Otherwise, no change to current state (still GameState.PLAYING).
-   }
-   
-   public void updateGame(int rowSelected, int colSelected) {
+	public void updateState(Seed theSeed, int rowSelected, int colSelected) {
+		if (hasWon(theSeed, rowSelected, colSelected)) {  // check for win
+			currentState = (theSeed == Seed.CROSS) ? GameState.CROSS_WON : GameState.NOUGHT_WON;
+		} else if (isDraw()) {  // check for draw
+			currentState = GameState.DRAW;
+		}
+		// Otherwise, no change to current state (still GameState.PLAYING).
+	}
+
+	public boolean isCellCross(int row, int col) {
+		return board[row][col] == Seed.CROSS;
+	}
+
+	public boolean isCellNought(int row, int col) {
+		return board[row][col] == Seed.NOUGHT;
+	}
+
+
+
+	public void updateGame(int rowSelected, int colSelected) {
 
 		if (currentState == GameState.PLAYING) {
 			if (rowSelected >= 0 && rowSelected < ROWS && colSelected >= 0
@@ -69,37 +79,44 @@ public class Game {
 		} else {       // game over
 			initGame(); // restart the game
 		}
-   }
-   
-   public boolean isDraw() {
-	      for (int row = 0; row < ROWS; ++row) {
-	         for (int col = 0; col < COLS; ++col) {
-	            if (board[row][col] == Seed.EMPTY) {
-	               return false; // an empty cell found, not draw, exit
-	            }
-	         }
-	      }
-	      return true;  // no more empty cell, it's a draw
-	   }
-   
-   public boolean hasWon(Seed theSeed, int rowSelected, int colSelected) {
-	      return (board[rowSelected][0] == theSeed  // 3-in-the-row
-	            && board[rowSelected][1] == theSeed
-	            && board[rowSelected][2] == theSeed
-	       || board[0][colSelected] == theSeed      // 3-in-the-column
-	            && board[1][colSelected] == theSeed
-	            && board[2][colSelected] == theSeed
-	       || rowSelected == colSelected            // 3-in-the-diagonal
-	            && board[0][0] == theSeed
-	            && board[1][1] == theSeed
-	            && board[2][2] == theSeed
-	       || rowSelected + colSelected == 2  // 3-in-the-opposite-diagonal
-	            && board[0][2] == theSeed
-	            && board[1][1] == theSeed
-	            && board[2][0] == theSeed);
-	   }
-   
+	}
 
-   
-   
+	public boolean isDraw() {
+		for (int row = 0; row < ROWS; ++row) {
+			for (int col = 0; col < COLS; ++col) {
+				if (board[row][col] == Seed.EMPTY) {
+					return false; // an empty cell found, not draw, exit
+				}
+			}
+		}
+		return true;  // no more empty cell, it's a draw
+	}
+
+	public boolean hasWon(Seed theSeed, int rowSelected, int colSelected) {
+		return (board[rowSelected][0] == theSeed  // 3-in-the-row
+				&& board[rowSelected][1] == theSeed
+				&& board[rowSelected][2] == theSeed
+				|| board[0][colSelected] == theSeed      // 3-in-the-column
+				&& board[1][colSelected] == theSeed
+				&& board[2][colSelected] == theSeed
+				|| rowSelected == colSelected            // 3-in-the-diagonal
+				&& board[0][0] == theSeed
+				&& board[1][1] == theSeed
+				&& board[2][2] == theSeed
+				|| rowSelected + colSelected == 2  // 3-in-the-opposite-diagonal
+				&& board[0][2] == theSeed
+				&& board[1][1] == theSeed
+				&& board[2][0] == theSeed);
+	}
+
+	public GameState getCurrentState() {
+		return currentState;
+	}
+
+	public Seed getCurrentPlayer() {
+		return currentPlayer;
+	}
+
+
+
 }
--- src/ttt/GameBoard.java
+++ src/ttt/GameBoard.java
@@ -1,5 +1,176 @@
 package ttt;
 
-public class GameBoard {
+import java.awt.BasicStroke;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+import ttt.Game.GameState;
+import ttt.Game.Seed;
+
+
+
+public class GameBoard extends JFrame{
+
+	public static  int ROWS;
+	public static  int COLS;
+	public static final int CELL_SIZE = 100; // cell width and height (square)
+	public static final int CANVAS_WIDTH = CELL_SIZE * COLS;  // the drawing canvas
+	public static final int CANVAS_HEIGHT = CELL_SIZE * ROWS;
+	public static final int GRID_WIDTH = 8;                   // Grid-line's width
+	public static final int GRID_WIDHT_HALF = GRID_WIDTH / 2; // Grid-line's half-width
+	// Symbols (cross/nought) are displayed inside a cell, with padding from border
+	public static final int CELL_PADDING = CELL_SIZE / 6;
+	public static final int SYMBOL_SIZE = CELL_SIZE - CELL_PADDING * 2; // width/height
+	public static final int SYMBOL_STROKE_WIDTH = 8; // pen's stroke width
+	private DrawCanvas canvas; // Drawing canvas (JPanel) for the game board
+	private JLabel statusBar;  // Status Bar
+	private Game gg;
+
+
+	public GameBoard() {
+
+		gg = new Game();
+		this.ROWS = gg.getRows();
+		this.COLS = gg.getCols();
+		// The canvas (JPanel) fires a MouseEvent upon mouse-click
+
+
+		// Setup the status bar (JLabel) to display status message
+
+		initGUI();
+
+
+	}
+
+	public void initGUI() {
+		canvas = new DrawCanvas();  // Construct a drawing canvas (a JPanel)
+		canvas.setPreferredSize(new Dimension(CANVAS_WIDTH, CANVAS_HEIGHT));
+		canvas.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {  // mouse-clicked handler
+				handleMouseClick(e);
+				// Refresh the drawing canvas
+				repaint();  // Call-back paintComponent().
+			}
+		});
+		createStatusBar();
+
+		Container cp = getContentPane();
+		cp.setLayout(new BorderLayout());
+		cp.add(canvas, BorderLayout.CENTER);
+		cp.add(statusBar, BorderLayout.PAGE_END); // same as SOUTH
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		pack();  // pack all the components in this JFrame
+		setTitle("Tic Tac Toe");
+		setVisible(true);  // show this JFrame
+	}
+
+	public void createStatusBar() {
+		statusBar = new JLabel("  ");
+		statusBar.setFont(new Font(Font.DIALOG_INPUT, Font.BOLD, 15));
+		statusBar.setBorder(BorderFactory.createEmptyBorder(2, 5, 4, 5));
+	}
+
+	public void handleMouseClick(MouseEvent e) {
+		int mouseX = e.getX();
+		int mouseY = e.getY();
+		// Get the row and column clicked
+		int rowSelected = mouseY / CELL_SIZE;
+		int colSelected = mouseX / CELL_SIZE;
+		gg.updateGame(rowSelected, colSelected);
+	}
+
+	class DrawCanvas extends JPanel {
+		@Override
+		public void paintComponent(Graphics g) {  // invoke via repaint()
+			super.paintComponent(g);    // fill background
+			setBackground(Color.WHITE); // set its background color
+			// Draw the grid-lines
+			drawGridLines(g);		
+			drawSeeds(g);
+			drawStatusBar();
+
+		}
+		private void drawGridLines(Graphics g) {
+			g.setColor(Color.LIGHT_GRAY);
+			for (int row = 1; row < ROWS; ++row) {
+				g.fillRoundRect(0, CELL_SIZE * row - GRID_WIDHT_HALF,
+						CANVAS_WIDTH-1, GRID_WIDTH, GRID_WIDTH, GRID_WIDTH);
+			}
+			for (int col = 1; col < COLS; ++col) {
+				g.fillRoundRect(CELL_SIZE * col - GRID_WIDHT_HALF, 0,
+						GRID_WIDTH, CANVAS_HEIGHT-1, GRID_WIDTH, GRID_WIDTH);
+			}
+		}
+
+		private void drawSeeds(Graphics g) {
+			// Draw the Seeds of all the cells if they are not empty
+			// Use Graphics2D which allows us to set the pen's stroke
+			Graphics2D g2d = (Graphics2D)g;
+			g2d.setStroke(new BasicStroke(SYMBOL_STROKE_WIDTH, BasicStroke.CAP_ROUND,
+					BasicStroke.JOIN_ROUND));  // Graphics2D only
+			for (int row = 0; row < ROWS; ++row) {
+				for (int col = 0; col < COLS; ++col) {
+					int x1 = col * CELL_SIZE + CELL_PADDING;
+					int y1 = row * CELL_SIZE + CELL_PADDING;
+					if (gg.isCellCross(row, col)) {
+						g2d.setColor(Color.RED);
+						int x2 = (col + 1) * CELL_SIZE - CELL_PADDING;
+						int y2 = (row + 1) * CELL_SIZE - CELL_PADDING;
+						g2d.drawLine(x1, y1, x2, y2);
+						g2d.drawLine(x2, y1, x1, y2);
+					} else if (gg.isCellNought(row, col)) {
+						g2d.setColor(Color.BLUE);
+						g2d.drawOval(x1, y1, SYMBOL_SIZE, SYMBOL_SIZE);
+					}
+				}
+			}
+		}
+
+		private void drawStatusBar() {
+			// Print status-bar message
+			if (gg.getCurrentState() == GameState.PLAYING) {
+				statusBar.setForeground(Color.BLACK);
+				if (gg.getCurrentPlayer() == Seed.CROSS) {
+					statusBar.setText("X's Turn");
+				} else {
+					statusBar.setText("O's Turn");
+				}
+			} else if (gg.getCurrentState() == GameState.DRAW) {
+				statusBar.setForeground(Color.RED);
+				statusBar.setText("It's a Draw! Click to play again.");
+			} else if (gg.getCurrentState() == GameState.CROSS_WON) {
+				statusBar.setForeground(Color.RED);
+				statusBar.setText("'X' Won! Click to play again.");
+			} else if (gg.getCurrentState() == GameState.NOUGHT_WON) {
+				statusBar.setForeground(Color.RED);
+				statusBar.setText("'O' Won! Click to play again.");
+			}
+		}
+
+	}
+
+	public static void main(String[] args) {
+		// Run GUI codes in the Event-Dispatching thread for thread safety
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				new GameBoard(); // Let the constructor do the job
+			}
+		});
+	}
 }
